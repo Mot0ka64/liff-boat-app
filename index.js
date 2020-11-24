d59b08ff52cc2f3920d7b23a2d9cfe319fef2f9c
Mot0ka64 <motoka64@gmail.com>
2020-11-24 20:15:12 +0900

    新歓スケジュールを入力

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 $(function () {
-    // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var number = $('input[name="number"]:checked').val();
-        var names = '';
-        $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
-        })
-        names = names.slice(0, -1);
+        var names = $('input[name="name"]').val();
 
-        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+        var msg = `希望日：${date}\n氏名：${names}`;
         sendText(msg);
 
         return false;
