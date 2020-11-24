cfac75be9c1601cbaa22fe41d283be5b0e688080
Mot0ka64 <motoka64@gmail.com>
2020-11-24 20:47:24 +0900

    日付の取得 fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function () {
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
+        var date = $('[name="date"] option:selected').val();
         var names = $('input[name="name"]').val();
 
         var msg = `希望日：${date}\n氏名：${names}`;
